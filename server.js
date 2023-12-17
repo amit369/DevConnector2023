4275d85fbadbeaacfdbd0c43e4428ba4e1f62379
amit-chaman <amitchaman369@gmail.com>
2023-12-17 18:35:03 +0530

    Added changes

--- server.js
+++ server.js
@@ -8,6 +8,9 @@ const PORT = process.env.PORT || 5000;
 // Connect Database
 connectDB();
 
+// Init Middleware
+app.use(express.json({ extended : false}));
+
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
